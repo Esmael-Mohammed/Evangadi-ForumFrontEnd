66def096d2a9064e829b51f92c58ab95895d81a8
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2024-12-22 21:19:48 +0300

    bulid

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing/Landing";
-import Home from "./pages/Home/Home";
+import Landing from "./Pages/Landing/Landing";
+import Home from "./Pages/Home/Home";
 import Question from "./Pages/Question/Question";
-import Answer from "./pages/Answer/Answer";
+import Answer from "./Pages/Answer/Answer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SharedLayout from "./components/sharedLayout/SharedLayout";
