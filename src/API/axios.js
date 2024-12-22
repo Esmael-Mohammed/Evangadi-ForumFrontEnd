01107f1637a4a90fa98497c1c348f9199a95178d
Esmael-Mohammed <esmaelmohammed01@gmail.com>
2024-12-22 22:29:28 +0300

    bulid

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   //local baseURL
-  //   baseURL: "http://localhost:3003/api",
+    // baseURL: "http://localhost:3003/api",
   //deployed base url
   baseURL: "https://evangadi-forum-backend-mnq9.onrender.com/api",
 });
